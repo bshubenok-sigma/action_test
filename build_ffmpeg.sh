61e3aa6b4b49599641ee0a120c928c968d2df0ad
Bohdan Shubenok <bohdan.shubenok@sigma.software>
2023-09-22 11:07:52 +0300

    Matrix builds

---
+++ build_ffmpeg.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+set -e
+
+# Source the ffmpeg configuration from the configuration file
+source ffmpeg_configuration
+
+# Settings
+FFMPEG_DIR=$1
+OPENSSL_DIR=$2
+TARGET=$3
+
+
+case $TARGET in
+    ios-x86-64)
+        PLATFORM=iphonesimulator
+        ARCH="x86_64"
+        CC="xcrun --sdk $PLATFORM clang"
+        SYSROOT=$(xcrun --sdk $TARGET --show-sdk-path)
+        ;;
+    ios-arm64)
+        PLATFORM=iphoneos
+        ARCH="arm64"
+        CC="xcrun --sdk $PLATFORM clang"
+        SYSROOT=$(xcrun --sdk $TARGET --show-sdk-path)
+        ;;
+    # android-arm64-v8a)
+    #     ;;
+    # android-armeabi-v7a)
+    #     ;;
+    # android-x86-64)
+    #     ;;
+    *)
+        echo "Unknown target: $TARGET"
+        exit 1
+        ;;
+esac
+
+# cd into ffmpeg root directory and build ffmpeg
+pushd $FFMPEG_DIR
+        ./configure $CONFIGURATION \
+                --cc="$CC" \
+                --arch="$ARCH" \
+                --sysroot="$SYSROOT" \
+                --prefix="build" \
+                --extra-cflags="-I$OPENSSL_DIR/include" \
+                --extra-ldflags="-L$OPENSSL_DIR"
+        make -j3
+        make install
+popd
---
+++ build_openssl.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -e
+
+OPENSSL_DIR=$1
+TARGET=$2
+
+cd OPENSSL_DIR
+
+case $TARGET in
+    ios-x86-64)
+        ./Configure iossimulator-xcrun no-ssl2 no-shared
+        ;;
+    ios-arm64)
+        ./Configure ios64-xcrun no-ssl2 no-shared
+        ;;
+    # android-arm64-v8a)
+    #     ./Configure ios64-xcrun no-ssl2 no-shared
+    #     ;;
+    # android-armeabi-v7a)
+    #     ./Configure ios64-xcrun no-ssl2 no-shared
+    #     ;;
+    # android-x86-64)
+    #     ./Configure android-x86_64 no-ssl2 no-shared
+    #     ;;
+    *)
+        echo "Unknown target: $TARGET"
+        exit 1
+        ;;
+esac
+
+MAKEFLAGS=silent make -j2
